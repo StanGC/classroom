16ee88786b8a611d321d2009f4a05dc898221cf0
Stan <solve153@gmail.com>
2016-11-27 15:47:21 +0800

    finish rspec course#show

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,18 +2,32 @@ require 'rails_helper'
 
 RSpec.describe CoursesController, type: :controller do
   describe "Get index" do
-  	it "assigns @courses and render" do
-  	  course1 = FactoryGirl.create(:course)
-  	  course2 = FactoryGirl.create(:course)
-  	  get :index
-  	  expect(assigns[:courses]).to eq([course1, course2])
-  	end
+    it "assigns @courses and render" do
+      course1 = FactoryGirl.create(:course)
+      course2 = FactoryGirl.create(:course)
+      get :index
+      expect(assigns[:courses]).to eq([course1, course2])
+    end
 
-  	it "render template" do
-  	  course1 = FactoryGirl.create(:course)
-  	  course2 = FactoryGirl.create(:course)
-  	  get :index
-  	  expect(response).to render_template("index")
-  	end
+    it "render template" do
+      course1 = FactoryGirl.create(:course)
+      course2 = FactoryGirl.create(:course)
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET show" do
+    it "assigns @course" do
+      course = FactoryGirl.create(:course)
+      get :show, params: { id: course.id }
+      expect(assigns[:course]).to eq(course)
+    end
+
+    it "render template" do
+      course = FactoryGirl.create(:course)
+      get :show, params: { id: course.id }
+      expect(response).to render_template("show")
+    end
   end
 end
