55002b82772950ae4a825ef65203965b479a68af
Stan <solve153@gmail.com>
2016-11-27 16:14:12 +0800

    finish rspec course#create

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -45,4 +45,26 @@ RSpec.describe CoursesController, type: :controller do
   	  expect(response).to render_template("new")
   	end
   end
+
+  describe "POST create" do
+  	it "doesn't create a record when course doesn't have a title" do
+  	  expect { post :create, params: { course: {:descirptoin => "bar"}}}.to change{Course.count}.by(0)
+  	end
+
+  	it "render new template when course doesn't have title" do
+  	  post :create, params:{ course: {:description => "bar"}}
+  	  expect(response).to render_template("new")
+  	end
+
+  	it "create a new course record when course has title" do
+  	  course = FactoryGirl.build(:course)
+  	  expect{ post :create, :course => FactoryGirl.attributes_for(:course)}.to change{ Course.count }.by(1)
+  	end
+
+  	it "redirect to courses_path when course has title" do
+  	  course = FactoryGirl.build(:course)
+  	  post :create, :course => FactoryGirl.attributes_for(:course)
+  	  expect(response).to redirect_to courses_path
+  	end
+  end
 end
