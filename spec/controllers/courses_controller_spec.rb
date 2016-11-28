28888d33312132f85ae3bb2e352d88ac28a234d5
Stan <solve153@gmail.com>
2016-11-28 23:31:38 +0800

    finish rspec course#new (user have to login)

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -32,18 +32,31 @@ RSpec.describe CoursesController, type: :controller do
   end
 
   describe "GET new" do
-    it "assign @course" do
-      course = FactoryGirl.build(:course)
-      get :new
-      expect(assigns(:course)).to be_new_record
-      expect(assigns(:course)).to be_instance_of(Course)
-  	end
+    context "when user login" do
+      it "assign @course" do
+        user = FactoryGirl.create(:user)
+        sign_in user
+        course = FactoryGirl.build(:course)
+        get :new
+        expect(assigns(:course)).to be_new_record
+        expect(assigns(:course)).to be_instance_of(Course)
+    	end
 
-  	it "render template" do
-  	  course = FactoryGirl.build(:course)
-  	  get :new
-  	  expect(response).to render_template("new")
-  	end
+    	it "render template" do
+        user = FactoryGirl.create(:user)
+        sign_in user
+    	  course = FactoryGirl.build(:course)
+    	  get :new
+    	  expect(response).to render_template("new")
+    	end
+    end
+
+    context "when user not login" do
+      it "redirect_to new_user_session_path" do
+        get :new
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 
   describe "POST create" do
